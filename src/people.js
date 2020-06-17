30449a0e55333d8076d250b9cc608540e3d45481
Gcrouzet <58287057+Gcrouzet@users.noreply.github.com>
2020-06-17 15:05:21 +0200

    people update

--- src/people.js
+++ src/people.js
@@ -20,10 +20,9 @@ class People {
 
     create(_people) {
 
-        if (this.isValid(_people)) {
+        if (this.isValid(_people) && this.people.find(e => e.id === people.id) === undefined && this.people.length < 256) {
             this.people.push(_people);
         }
-
         return _employee;
     }
 
@@ -33,20 +32,38 @@ class People {
 
         if (pers !== undefined) {
 
-            let persClone = Object.assign({}, emp)
-            return pers2
+            let persClone = Object.assign(new Person(), pers)
+            return persClone;
         }
         return undefined;
     }
 
-    update() {
+    update(_people) {
+        if (!this.isValid(_people)) {
+            return _people;
+        }
 
+
+        if (_people.id < 1 && _people.id !== NaN) {
+            return _people;
+        }
+
+        let exists = this.people.find(e => e.id === _people.id);
+
+        if (exists !== undefined) {
+            exists.copy(_people);
+            return exists;
+        }
+
+        return exists;
     }
+
 
     delete(_id) {
-        let employeeIndex = this.employees.findIndex(emp => emp.id === parseInt(_id));
-        this.employees.splice(employeeIndex, 1);
+        let toDelete = this.people.findIndex(emp => emp.id === parseInt(_id));
+        if (toDelete > -1)
+            this.people.splice(toDelete, 1);
     }
 }
-    
 
+module.exports = People ;
